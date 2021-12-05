aacbe3d7ef278ff324387f707ca2aead2119c872
FellipeLorram <83411249+FellipeLorram@users.noreply.github.com>
2021-12-05 10:08:19 -0300

    Add files via upload

---
+++ GeraSenha/src/modules/GeraSenha.js
@@ -0,0 +1,12 @@
+class GeraSenha{
+    constructor(){
+        this.Quantidade = document.querySelector('#QuantidadeCaracteres');
+    }
+    senhaNumerica(){
+        
+    }
+
+    static rand(min , max, letraMaiuscula = null, letraMinuscula = null, simbolos = null ){
+        
+    }
+}
